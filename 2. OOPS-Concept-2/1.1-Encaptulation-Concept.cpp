55392a61d47f89acf49e7075c64faaed7f4032e8
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-07-23 01:06:03 +0530

    OOPS-Concept-2

---
+++ 2. OOPS-Concept-2/1.1-Encaptulation-Concept.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#include<iostream>
+using namespace std;
+
+class Student {
+
+    private:
+        string name;
+        int age;
+        int height;
+
+    public:
+    int getAge() {
+        return this->age;
+    }
+};
+
+int main() {
+
+    Student first;
+
+    cout << "Sab sahi chalra hai" << endl;
+
+    return 0;
+}
+
+
+
+// Abstraction vs Encapsulation Stack Overflow: https://stackoverflow.com/questions/7...
+
+
+// CodeStudio OOPs Reference: https://bit.ly/3tMl5mZ
